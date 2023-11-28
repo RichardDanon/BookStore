d45f23b67ad2ce8abc64418f49b3da2a03f1a67e
Richard Danon-Woedey <104465518+RichardDanon@users.noreply.github.com>
2023-11-28 14:10:09 -0500

    View Authors Completed (#4)
    * View Authors Completed
    
    * Mini Changes

---
+++ BookStoreApp_project_initial/Controllers/AuthorController.cs
@@ -0,0 +1,36 @@
+﻿using BookStoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Controllers
+{
+	public class AuthorController : Controller
+	{
+		private readonly BookstoreContext context;
+
+		public AuthorController(BookstoreContext ctx)
+		{
+			context = ctx;
+		}
+
+		// GET: Author
+		public IActionResult Index(int page = 1, int pageSize = 5)
+		{
+			int skip = (page - 1) * pageSize;
+			var authorsQuery = context.Authors.OrderBy(a => a.LastName);
+			var totalCount = authorsQuery.Count();
+			var authors = authorsQuery.Skip(skip).Take(pageSize).ToList();
+
+			var pagedList = new PagedList<Author>
+			{
+				Items = authors,
+				PageNumber = page,
+				PageSize = pageSize,
+				TotalCount = totalCount
+			};
+
+			return View(pagedList);
+		}
+
+	}
+}
