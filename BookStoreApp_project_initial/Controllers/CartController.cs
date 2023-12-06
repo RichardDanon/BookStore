648845432f4c2b61e1b87bbfd01db99ccca68764
Youssef Chaboune <Chahbouney2000@outlook.fr>
2023-12-06 06:06:06 -0500

    Implemented cart functionalities

---
+++ BookStoreApp_project_initial/Controllers/CartController.cs
@@ -0,0 +1,136 @@
+﻿using BookStoreApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+using System.Text.Json;
+
+namespace BookStoreApp.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+
+        private UserManager<User> userManager;
+        private BookstoreContext context;
+        private SignInManager<User> signInManager;
+
+        public CartController(BookstoreContext ctx,UserManager<User> userMngr,
+            SignInManager<User> signInMngr)
+        {
+            userManager = userMngr;
+            signInManager = signInMngr;
+            context = ctx;
+        }
+
+        public IActionResult CartView()
+        {
+            
+            var userCart = GetUserCartFromCookie();
+
+            List<Book> books = context.Books.Include(b => b.authorObject).Include(b => b.Genre).ToList();
+            List<Book> userCartBooks = null;
+
+            if(userCart != null)
+            {
+                userCartBooks = new List<Book>();
+                foreach (string bookId in userCart.BooksIds) {
+                
+                    foreach (Book b in  books)
+                    {
+                        if(int.Parse(bookId) == b.BookId)
+                        {
+                            userCartBooks.Add(b);
+                        }
+
+                    }
+ 
+                }
+
+                
+            }
+            
+            return View(userCartBooks);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToCart(int id)
+        {
+
+            var username = User.Identity.Name;
+
+            var userCart = GetUserCartFromCookie();
+
+            if (userCart != null)
+            {
+                if (!userCart.BooksIds.Contains(id.ToString()))
+                {
+                    // Add the new productId to the list
+                    userCart.BooksIds.Add(id.ToString());
+
+                    // Update the user's cart in the cookie
+                    SaveUserCartToCookie(userCart);
+                }
+
+            } else
+            {
+                userCart = new UserCart
+                {
+                    UserName = username,
+                    BooksIds = new List<string> { id.ToString() }
+                };
+
+                SaveUserCartToCookie(userCart);
+            }
+
+            return NoContent();
+
+
+        }
+
+        private UserCart GetUserCartFromCookie()
+        {
+            var cookieValue = Request.Cookies[$"UserCartCookie_{User.Identity.Name}"];
+            return cookieValue != null ? JsonSerializer.Deserialize<UserCart>(cookieValue) : null;
+        }
+
+        private void SaveUserCartToCookie(UserCart userCart)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(30),
+                HttpOnly = true,
+                Secure = true
+            };
+
+            var cookieValue = JsonSerializer.Serialize(userCart);
+            Response.Cookies.Append($"UserCartCookie_{userCart.UserName}", cookieValue, cookieOptions);
+        }
+
+        public IActionResult RemoveBookFromCart(int id)
+        {
+            var userCart = GetUserCartFromCookie();
+
+            if (userCart != null && userCart.BooksIds.Contains(id.ToString()))
+            {
+                
+                // Add the new productId to the list
+                userCart.BooksIds.Remove(id.ToString());
+
+                // Update the user's cart in the cookie
+                SaveUserCartToCookie(userCart);
+
+                return RedirectToAction("CartView", "Cart");
+                
+
+            }
+            else { 
+                return Json(new { error = "Error somthing happend" });
+            }
+            
+        }
+
+
+    }
+}
---
+++ BookStoreApp_project_initial/Models/UserCart.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreApp.Models
+{
+    public class UserCart
+    {
+        public string UserName { get; set; }
+
+        public List<string> BooksIds { get; set; }
+    }
+}
