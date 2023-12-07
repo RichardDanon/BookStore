754c4ac5f2e94929b9d52e6bb912fc5fde98253b
Richard Danon-Woedey <richard2004danon@gmail.com>
2023-12-06 22:58:35 -0500

    Re did get authors with proper view model

--- BookStoreApp_project_initial/Controllers/AuthorController.cs
+++ BookStoreApp_project_initial/Controllers/AuthorController.cs
@@ -1,38 +1,105 @@
 ﻿using BookStoreApp.Models;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreApp.Controllers
 {
-	[Authorize]
-	public class AuthorController : Controller
-	{
-		private readonly BookstoreContext context;
+    public class AuthorController : Controller
+    {
+        private BookstoreContext context { get; set; }
 
-		public AuthorController(BookstoreContext ctx)
-		{
-			context = ctx;
-		}
+        public AuthorController(BookstoreContext ctx) => context = ctx;
 
-		// GET: Author
-		public IActionResult Index(int page = 1, int pageSize = 5)
-		{
-			int skip = (page - 1) * pageSize;
-			var authorsQuery = context.Authors.OrderBy(a => a.LastName);
-			var totalCount = authorsQuery.Count();
-			var authors = authorsQuery.Skip(skip).Take(pageSize).ToList();
+        // Get Authors
+        public IActionResult Index(int page = 1, int pageSize = 5, string searchTerm = "")
+        {
+            int skip = (page - 1) * pageSize;
 
-			var pagedList = new PagedList<Author>
-			{
-				Items = authors,
-				PageNumber = page,
-				PageSize = pageSize,
-				TotalCount = totalCount
-			};
+            var authorsQuery = context.Authors.AsQueryable();
 
-			return View(pagedList);
-		}
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                // Apply search filter
+                authorsQuery = authorsQuery
+                    .Where(a => EF.Functions.Like(a.FirstName, $"%{searchTerm}%") || EF.Functions.Like(a.LastName, $"%{searchTerm}%"));
+            }
 
-	}
+            authorsQuery = authorsQuery.OrderBy(m => m.AuthorId);
+
+            var totalCount = authorsQuery.Count();
+            var authors = authorsQuery.Skip(skip).Take(pageSize).ToList();
+
+            var pagedList = new PagedList<Author>
+            {
+                Items = authors,
+                PageNumber = page,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+
+            var viewModel = new AuthorViewModel
+            {
+                PageResult = pagedList,
+            };
+
+            return View(viewModel);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var author = context.Authors.Find(id);
+            return View(author);
+        }
+        [HttpPost]
+        public IActionResult Edit(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                if (author.AuthorId == 0)
+                    context.Authors.Add(author);
+                else
+                    context.Authors.Update(author);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Author");
+            }
+            else
+            {
+                return View(author);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var author = context.Authors.Find(id);
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Author author)
+        {
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            var booksQuery = context.Books
+                .Where(b => b.AuthorId == author.AuthorId)
+                .ToList();
+
+            context.Books.RemoveRange(booksQuery);
+
+            context.Authors.Remove(author);
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Author");
+        }
+
+    }
 }
