43cc165528958df0e86b7099114f18e5fdc64bd7
Caleb Liu <liucaleb8@gmail.com>
2023-12-07 01:29:55 -0500

    Implement_Valid_Book_ISBN

--- BookStoreApp_project_initial/Models/BookstoreContext.cs
+++ BookStoreApp_project_initial/Models/BookstoreContext.cs
@@ -173,22 +173,22 @@ namespace BookStoreApp.Models
 
 
 
-				new Book
-				{
-					BookId = 16,
-					GenreId = 1,
-					AuthorId = 14,
-					Title = "To Kill a Mockingbird",
-					ISBN = "00000006",
-					Price = 10.99
-				},
+                new Book
+                {
+                    BookId = 16,
+                    GenreId = 1,
+                    AuthorId = 14,
+                    Title = "To Kill a Mockingbird",
+                    ISBN = "978-0471234567",
+                    Price = 10.99
+                },
 				new Book
 				{
 					BookId = 17,
 					GenreId = 3,
 					AuthorId = 15,
 					Title = "Romeo and Juliet",
-					ISBN = "00000007",
+					ISBN = "978-0472345671",
 					Price = 16.99
 				},
 				new Book
@@ -197,7 +197,7 @@ namespace BookStoreApp.Models
 					GenreId = 4,
 					AuthorId = 16,
 					Title = "The Shining",
-					ISBN = "00000008",
+					ISBN = "978-0473456789",
 					Price = 35.78
 				},
 				new Book
@@ -206,7 +206,7 @@ namespace BookStoreApp.Models
 					GenreId = 6,
 					AuthorId = 17,
 					Title = "A People's History of the United States",
-					ISBN = "00000009",
+					ISBN = "978-9474567890",
 					Price = 24.09
 				},
 				new Book
@@ -215,7 +215,7 @@ namespace BookStoreApp.Models
 					GenreId = 2,
 					AuthorId = 18,
 					Title = "Dune",
-					ISBN = "00000010",
+					ISBN = "978-0475678901",
 					Price = 19.99
 				},
 				new Book
@@ -224,7 +224,7 @@ namespace BookStoreApp.Models
 					GenreId = 1,
 					AuthorId = 19,
 					Title = "Pride and Prejudice",
-					ISBN = "00000011",
+					ISBN = "978-0476789012",
 					Price = 12.99
 				},
 				new Book
@@ -233,7 +233,7 @@ namespace BookStoreApp.Models
 					GenreId = 3,
 					AuthorId = 15,
 					Title = "Hamlet",
-					ISBN = "00000012",
+					ISBN = "978-0477890123",
 					Price = 50.89
 				},
 				new Book
@@ -242,7 +242,7 @@ namespace BookStoreApp.Models
 					GenreId = 4,
 					AuthorId = 20,
 					Title = "The Exorcist",
-					ISBN = "00000013",
+					ISBN = "978-0478901234",
 					Price = 14.99
 				},
 				new Book
@@ -251,7 +251,7 @@ namespace BookStoreApp.Models
 					GenreId = 6,
 					AuthorId = 21,
 					Title = "Sapiens: A Brief History of Humankind",
-					ISBN = "000000014",
+					ISBN = "978-0479012345",
 					Price = 15.99
 				},
 				new Book
@@ -260,7 +260,7 @@ namespace BookStoreApp.Models
 					GenreId = 2,
 					AuthorId = 22,
 					Title = "Neuromancer",
-					ISBN = "00000015",
+					ISBN = "978-0470123456",
 					Price = 21.25
 				},
 				new Book
@@ -269,7 +269,7 @@ namespace BookStoreApp.Models
 					GenreId = 1,
 					AuthorId = 23,
 					Title = "The Great Gatsby",
-					ISBN = "00000016",
+					ISBN = "978-0471234567",
 					Price = 60.99
 				},
 				new Book
@@ -278,7 +278,7 @@ namespace BookStoreApp.Models
 					GenreId = 3,
 					AuthorId = 15,
 					Title = "Macbeth",
-					ISBN = "00000017",
+					ISBN = "978-0472345678",
 					Price = 28
 				},
 				new Book
@@ -287,7 +287,7 @@ namespace BookStoreApp.Models
 					GenreId = 4,
 					AuthorId = 16,
 					Title = "It",
-					ISBN = "00000018",
+					ISBN = "978-0473456789",
 					Price = 16.50
 				},
 				new Book
@@ -296,7 +296,7 @@ namespace BookStoreApp.Models
 					GenreId = 6,
 					AuthorId = 14,
 					Title = "The Wright Brothers",
-					ISBN = "00000019",
+					ISBN = "978-0474567890",
 					Price = 32.99
 				},
 				new Book
@@ -305,7 +305,7 @@ namespace BookStoreApp.Models
 					GenreId = 2,
 					AuthorId = 15,
 					Title = "Foundation",
-					ISBN = "00000020",
+					ISBN = "978-0475678901",
 					Price = 13.99
 				},
 
@@ -313,43 +313,42 @@ namespace BookStoreApp.Models
 				{
 					BookId = 32,
 					Title = "The Lord of the Rings",
-					ISBN = "000092",
+					ISBN = "978-0476789012",
 					GenreId = 7,
 					AuthorId = 14,
 					Price = 14.99
 				},
 
-				 new Book
-				 {
-					 BookId = 33,
-					 Title = "The Silmarillion",
-					 ISBN = "000096",
-					 GenreId = 7,
-					 AuthorId = 14,
-					 Price = 17.99
-				 },
+				new Book
+				{
+					BookId = 33,
+					Title = "The Silmarillion",
+					ISBN = "978-0477890123",
+					GenreId = 7,
+					AuthorId = 14,
+					Price = 17.99
+				},
 
+				new Book
+				{
+					BookId = 34,
+					GenreId = 1,
+					AuthorId = 19,
+					Title = "Love in the Night",
+					ISBN = "978-0478901234",
+					Price = 18.99
+				},
+				new Book
+				{
+					BookId = 35,
+					GenreId = 1,
+					AuthorId = 23,
+					Title = "The Catcher in the Rye",
+					ISBN = "978-0479012345",
+					Price = 13.67
+				}
 
-				 new Book
-				 {
-					 BookId = 34,
-					 GenreId = 1,
-					 AuthorId = 19,
-					 Title = "Love in the Night",
-					 ISBN = "00000095",
-					 Price = 18.99
-				 },
-				 new Book
-				 {
-					 BookId = 35,
-					 GenreId = 1,
-					 AuthorId = 23,
-					 Title = "The Catcher in the Rye",
-					 ISBN = "00000097",
-					 Price = 13.67
-				 }
-
-			);
+            );
 
             modelBuilder.Entity<Author>().HasData(
 			   new Author
