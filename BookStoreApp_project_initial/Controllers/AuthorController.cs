248ffaaae9b60900b146a00b865b0bdb7250df06
Richard Danon-Woedey <104465518+RichardDanon@users.noreply.github.com>
2023-12-06 23:54:35 -0500

    Implement author has been completed (#8)
    * Re did get authors with proper view model
    
    * Finished Edit author
    
    * Delete done

--- BookStoreApp_project_initial/Controllers/AuthorController.cs
+++ BookStoreApp_project_initial/Controllers/AuthorController.cs
@@ -1,37 +1,132 @@
 ﻿using BookStoreApp.Models;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookStoreApp.Controllers
 {
-	[Authorize]
-	public class AuthorController : Controller
-	{
-		private readonly BookstoreContext context;
+    public class AuthorController : Controller
+    {
+        private BookstoreContext context { get; set; }
 
-		public AuthorController(BookstoreContext ctx)
+        public AuthorController(BookstoreContext ctx) => context = ctx;
+
+        // Get Authors
+        public IActionResult Index(int page = 1, int pageSize = 5, string searchTerm = "")
+        {
+            int skip = (page - 1) * pageSize;
+
+            var authorsQuery = context.Authors.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                // Apply search filter
+                authorsQuery = authorsQuery
+                    .Where(a => EF.Functions.Like(a.FirstName, $"%{searchTerm}%") || EF.Functions.Like(a.LastName, $"%{searchTerm}%"));
+            }
+
+            authorsQuery = authorsQuery.OrderBy(m => m.AuthorId);
+
+            var totalCount = authorsQuery.Count();
+            var authors = authorsQuery.Skip(skip).Take(pageSize).ToList();
+
+            var pagedList = new PagedList<Author>
+            {
+                Items = authors,
+                PageNumber = page,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+
+            var viewModel = new AuthorViewModel
+            {
+                PageResult = pagedList,
+            };
+
+            return View(viewModel);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var author = context.Authors.Find(id);
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("EditConfirmation", author);
+            }
+            else
+            {
+                return View(author);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult EditConfirmation(Author author)
+        {
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmEdit(Author author)
+        {
+            if (author.AuthorId == 0)
+                context.Authors.Add(author);
+            else
+                context.Authors.Update(author);
+
+            context.SaveChanges();
+            return RedirectToAction("Index", "Author");
+        }
+
+
+		[HttpGet]
+		public IActionResult Delete(int id)
 		{
-			context = ctx;
-		}
-
-		// GET: Author
-		public IActionResult Index(int page = 1, int pageSize = 5)
-		{
-			int skip = (page - 1) * pageSize;
-			var authorsQuery = context.Authors.OrderBy(a => a.LastName);
-			var totalCount = authorsQuery.Count();
-			var authors = authorsQuery.Skip(skip).Take(pageSize).ToList();
-
-			var pagedList = new PagedList<Author>
+			var author = context.Authors.Find(id);
+			if (author == null)
 			{
-				Items = authors,
-				PageNumber = page,
-				PageSize = pageSize,
-				TotalCount = totalCount
+				return NotFound();
+			}
+
+			// Fetch related books
+			var books = context.Books
+				.Where(b => b.AuthorId == id)
+				.ToList();
+
+			var viewModel = new AuthorDeleteViewModel
+			{
+				Author = author,
+				RelatedBooks = books
 			};
 
-			return View(pagedList);
+			return View("DeleteConfirmation", viewModel);
+		}
+
+		[HttpPost]
+		public IActionResult ConfirmDelete(int id)
+		{
+			var author = context.Authors.Find(id);
+			if (author == null)
+			{
+				return NotFound();
+			}
+
+			// Remove related books and author
+			var books = context.Books.Where(b => b.AuthorId == id).ToList();
+			context.Books.RemoveRange(books);
+			context.Authors.Remove(author);
+
+			context.SaveChanges();
+
+			return RedirectToAction("Index", "Author");
 		}
 
 	}
---
+++ BookStoreApp_project_initial/Models/AuthorDeleteViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStoreApp.Models
+{
+	public class AuthorDeleteViewModel
+	{
+		public Author Author { get; set; }
+		public List<Book> RelatedBooks { get; set; }
+	}
+}
